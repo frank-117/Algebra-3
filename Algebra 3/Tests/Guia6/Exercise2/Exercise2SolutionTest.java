b232d514e158c1c06ce47e025b32ad67e11fedf1
frank-117 <franco.velardez@ing.austral.edu.ar>
2017-05-21 16:19:57 -0300

    finished ex2

--- Algebra 3/Tests/Guia6/Exercise2/Exercise2SolutionTest.java
+++ Algebra 3/Tests/Guia6/Exercise2/Exercise2SolutionTest.java
@@ -1,3 +1,7 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°6
+// Ejercicio: 2)
+
 package Guia6.Exercise2;
 
 import Guia6.CalculatorAbstract.CalculatorAbstract;
@@ -9,13 +13,12 @@ public class Exercise2SolutionTest extends CalculatorAbstract {
 
     @Test
     public void exerciseA() throws Exception {
-
-        double[][] matrix1 = {{3, 2, 4}, {5, 7, 8}, {9, 2, 6}};
+        double[][] matrix1 = {{8, -1, 3}, {-1, 7, 4}, {3, 4, 9}};
         Exercise2Solution exercise2Solution1 = new Exercise2Solution();
         boolean result1 = exercise2Solution1.exerciseA(matrix1, calculator);
         assertTrue(result1);
 
-        double[][] matrix2 = {{3, 2, 4}, {5, 7, 8}, {9, 2, 6}};
+        double[][] matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         Exercise2Solution exercise2Solution2 = new Exercise2Solution();
         boolean result2 = exercise2Solution2.exerciseA(matrix2, calculator);
         assertFalse(result2);
@@ -23,5 +26,14 @@ public class Exercise2SolutionTest extends CalculatorAbstract {
 
     @Test
     public void exerciseB() throws Exception {
+        double[][] matrix1 = {{-4, 2, 1}, {2, -8, 4}, {1, -1, 6}};
+        Exercise2Solution exercise2Solution1 = new Exercise2Solution();
+        boolean result1 = exercise2Solution1.exerciseB(matrix1, calculator);
+        assertTrue(result1);
+
+        double[][] matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Exercise2Solution exercise2Solution2 = new Exercise2Solution();
+        boolean result2 = exercise2Solution2.exerciseB(matrix2, calculator);
+        assertFalse(result2);
     }
 }
--- Algebra 3/src/Guia6/Exercise2/Exercise2Solution.java
+++ Algebra 3/src/Guia6/Exercise2/Exercise2Solution.java
@@ -1,17 +1,55 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°6
+// Ejercicio: 2)
+
 package Guia6.Exercise2;
 
 import Guia6.Calculator.Calculator;
 
 public class Exercise2Solution implements Exercise2 {
 
+    /**
+     * @param matrix
+     * @param calculator
+     * @return TRUE if the given matrix is symmetrical, or FALSE otherwise
+     */
     @Override
     public boolean exerciseA(double[][] matrix, Calculator calculator) {
-
-        return false;
+        boolean result = true;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
+    /**
+     * @param matrix
+     * @param calculator
+     * @return TRUE if the given matrix is diagonally dominant, or FALSE otherwise
+     */
     @Override
     public boolean exerciseB(double[][] matrix, Calculator calculator) {
-        return false;
+        boolean result = true;
+        for (int i = 0; i < matrix.length; i++) {
+            double main = 0;
+            double rest = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    main = Math.abs(matrix[i][j]);
+                } else {
+                    rest = calculator.sum(rest, Math.abs(matrix[i][j]));
+                }
+            }
+            if (main < rest) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
