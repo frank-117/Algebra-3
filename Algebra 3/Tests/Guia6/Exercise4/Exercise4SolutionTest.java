64b0c42ded504ab15b9c110c15c9f9ff84c22c8e
frank-117 <franco.velardez@ing.austral.edu.ar>
2017-05-22 17:27:13 -0300

    finished ex4

--- Algebra 3/Tests/Guia6/Exercise4/Exercise4SolutionTest.java
+++ Algebra 3/Tests/Guia6/Exercise4/Exercise4SolutionTest.java
@@ -1,3 +1,7 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°6
+// Ejercicio: 4)
+
 package Guia6.Exercise4;
 
 import Guia6.CalculatorAbstract.CalculatorAbstract;
@@ -9,26 +13,25 @@ public class Exercise4SolutionTest extends CalculatorAbstract {
 
     @Test
     public void summation() throws Exception {
-        double[] matrixA = {5,6,7,9,5,2,4,1,3,2};
-        double[] matrixB = {2,6,1,3,7,3,8,3,8,2};
+        double[] matrixA = {5, 6, 7, 9, 5, 2, 4, 1, 3, 2};
+        double[] matrixB = {2, 6, 1, 3, 7, 3, 8, 3, 8, 2};
         Exercise4Solution exercise = new Exercise4Solution();
         double[][] result = exercise.summation(matrixA, matrixB, calculator);
-        assertEquals(result[0][0], 7 , 0.1);
-        assertEquals(result[0][1], 0 , 0.1);
-        assertEquals(result[0][2], 0 , 0.1);
-        assertEquals(result[0][3], 0 , 0.1);
-        assertEquals(result[1][0], 12, 0.1);
-        assertEquals(result[1][1], 12, 0.1);
-        assertEquals(result[1][2], 0 , 0.1);
-        assertEquals(result[1][3], 0 , 0.1);
-        assertEquals(result[2][0], 8 , 0.1);
-        assertEquals(result[2][1], 5 , 0.1);
-        assertEquals(result[2][2], 4 , 0.1);
-        assertEquals(result[2][3], 0 , 0.1);
-        assertEquals(result[3][0], 12, 0.1);
-        assertEquals(result[3][1], 12, 0.1);
-        assertEquals(result[3][2], 11, 0.1);
-        assertEquals(result[3][3], 4 , 0.1);
-
+        assertEquals(result[0][0], 7, 0);
+        assertEquals(result[0][1], 0, 0);
+        assertEquals(result[0][2], 0, 0);
+        assertEquals(result[0][3], 0, 0);
+        assertEquals(result[1][0], 12, 0);
+        assertEquals(result[1][1], 12, 0);
+        assertEquals(result[1][2], 0, 0);
+        assertEquals(result[1][3], 0, 0);
+        assertEquals(result[2][0], 8, 0);
+        assertEquals(result[2][1], 5, 0);
+        assertEquals(result[2][2], 4, 0);
+        assertEquals(result[2][3], 0, 0);
+        assertEquals(result[3][0], 12, 0);
+        assertEquals(result[3][1], 12, 0);
+        assertEquals(result[3][2], 11, 0);
+        assertEquals(result[3][3], 4, 0);
     }
 }
--- Algebra 3/src/Guia6/Exercise4/Exercise4Solution.java
+++ Algebra 3/src/Guia6/Exercise4/Exercise4Solution.java
@@ -1,21 +1,31 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°6
+// Ejercicio: 4)
+
 package Guia6.Exercise4;
 
 import Guia6.Calculator.Calculator;
 
 public class Exercise4Solution implements Exercise4 {
 
+    /**
+     * @param matrixA
+     * @param matrixB
+     * @param calculator
+     * @return the sum of both given matrices
+     */
     @Override
     public double[][] summation(double[] matrixA, double[] matrixB, Calculator calculator) {
-        int matrixLength = (int) matrixLength(matrixA.length);
+        int matrixLength = (int) matrixLength(matrixA.length, calculator);
         double[] sumVector = new double[matrixA.length];
         double[][] result = new double[matrixLength][matrixLength];
-        for(int i = 0; i<matrixA.length; i ++){
-            sumVector[i] =matrixA[i] + matrixB[i];
+        for(int i = 0; i < matrixA.length; i++){
+            sumVector[i] = calculator.sum(matrixA[i], matrixB[i]);
         }
         int counter= 0;
-        for(int i= 0; i<matrixLength ; i++){
-            for(int j = 0; j<matrixLength ; j++){
-                if(i<=j) {
+        for(int i = 0; i < matrixLength; i++){
+            for(int j = 0; j < matrixLength; j++){
+                if(i <= j) {
                     result[j][i] = sumVector[counter];
                     counter++;
                 }
@@ -24,12 +34,15 @@ public class Exercise4Solution implements Exercise4 {
         return result;
     }
 
-    private double matrixLength(int vectorLength){
-        double a = (- 1 + Math.sqrt(1 +8*vectorLength))/2;
-        double b = (- 1 - Math.sqrt(1 +8*vectorLength))/2;
-        if(a>0){
-            return a;
-        }
-        else return b;
+    /**
+     * @param vectorLength
+     * @param calculator
+     * @return the length of the matrix based on the length of its simplified form as a vector without the zeroes
+     */
+    private double matrixLength(int vectorLength, Calculator calculator) {
+        double a = calculator.division(calculator.sum(-1, Math.sqrt(calculator.sum(1, calculator.multiplication(8, vectorLength)))), 2);
+        double b = calculator.division(calculator.subtraction(-1, Math.sqrt(calculator.sum(1, calculator.multiplication(8, vectorLength)))), 2);
+        if (a > 0) return a;
+        return b;
     }
 }
