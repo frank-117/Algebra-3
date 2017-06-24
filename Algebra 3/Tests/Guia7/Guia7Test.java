93247c5ffc1ce469f328edb1d11903a01be6fa23
frank-117 <franco.velardez@ing.austral.edu.ar>
2017-06-24 00:35:01 -0300

    Finished Guia7

--- Algebra 3/Tests/Guia7/Guia7Test.java
+++ Algebra 3/Tests/Guia7/Guia7Test.java
@@ -1,10 +1,13 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°7
+
 package Guia7;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class Guia7Test {
+public class Guia7Test extends CalculatorAbstract {
 
     Guia7 guia7 = new Guia7();
 
@@ -21,6 +24,13 @@ public class Guia7Test {
 
     @Test
     public void exercise2() throws Exception {
+        double[][] matrix = { {1, 0, 0}, {2, 4, 0}, {2, 4, 6} };
+        double[] result = {1, 2, 14};
+        double[] gaussResult = guia7.exercise2(matrix, result);
+        double[] expectedResult = {1, 0, 2};
+        for (int i = 0; i < gaussResult.length; i++) {
+            assertEquals(expectedResult[i], gaussResult[i], 0);
+        }
     }
 
     @Test
@@ -45,17 +55,43 @@ public class Guia7Test {
 
     @Test
     public void exercise6() throws Exception {
+        double[][] matrix = { {1, 2, 4, 6}, {6, 4, 2, 1}, {0, 1, 1, 2}, {0, 0, 8, 2} };
+        double[] result = {1, 2, 1, 2};
+        double[] gaussResult = guia7.exercise6(matrix, result, calculator);
+        for (int i = 0; i < gaussResult.length; i++) {
+            System.out.println("X" + (i + 1) + ": " + gaussResult[i]);
+        }
     }
 
     @Test
     public void exercise7() throws Exception {
+        double[][] matrix = { {1, 4, 0, 0}, {4, 2, 6, 0}, {0, 6, 3, 8}, {0, 0, 8, 4} };
+        double[] result = {1, 2, 1, 2};
+        double[] gaussResult = guia7.exercise7(matrix, result, calculator);
+        for (int i = 0; i < gaussResult.length; i++) {
+            System.out.println("X" + (i + 1) + ": " + gaussResult[i]);
+        }
     }
 
     @Test
     public void exercise8() throws Exception {
+        double[][] matrix = { {1, 2}, {3, 5} };
+        double[][] result = guia7.exercise8(matrix);
+        for (double[] aMatrix : result) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(aMatrix[j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     @Test
     public void exercise9() throws Exception {
+        double[][] matrix = { {0.5, 1.5, 0.7}, {1, 0, 2}, {2, 1.2, -0.3} };
+        double[] result = {-0.3, 3, 0.5};
+        double[] gaussResult = guia7.exercise9(matrix, result);
+        for (int i = 0; i < gaussResult.length; i++) {
+            System.out.println("X" + (i + 1) + ": " + gaussResult[i]);
+        }
     }
 }
--- Algebra 3/src/Guia7/Guia7.java
+++ Algebra 3/src/Guia7/Guia7.java
@@ -1,37 +1,78 @@
+// Nombres: Tomas Varela, Franco Velardez
+// Guía: N°7
+
 package Guia7;
 
 public class Guia7 implements TP4 {
 
+    /**
+     * @param coefficients upper triangular matrix with 1 in the main diagonal
+     * @param independentTerms results
+     * @return the solutions to the equations system
+     */
     @Override
     public double[] exercise1(double[][] coefficients, double[] independentTerms) {
-        double[] result = new double[coefficients.length];
-        for (int i = coefficients.length - 1; i > 0; i--) {
-            for (int j = coefficients[i].length - 1; j >= i; j--) {
-                result[i] = independentTerms[i];
-                int end = i;
-                while (end != coefficients.length && (end - 1) > coefficients[i].length) {
-                    result[i] -= coefficients[end - 1][coefficients[end].length - 1] * coefficients[end][coefficients[end].length - 1];
-                    end--;
-                }
+        double[] result = new double[independentTerms.length];
+        for(int i = coefficients.length - 1; i >= 0; i--){
+            double value = 0;
+            for(int j = i + 1; j < coefficients.length; j++){
+                value += coefficients[i][j] * result[j];
             }
+            result[i] = independentTerms[i] - value;
         }
         return result;
     }
 
+    /**
+     * @param coefficients lower triangular matrix
+     * @param independentTerms results
+     * @return the solutions to the equations system
+     */
     @Override
     public double[] exercise2(double[][] coefficients, double[] independentTerms) {
-        return new double[0];
+        double[] result = new double[independentTerms.length];
+        for(int i = 0; i < coefficients.length; i++){
+            double value = 0;
+            for(int j = 0; j < i; j++){
+                value += coefficients[i][j] * result[j];
+            }
+            result[i] = (independentTerms[i] - value) / coefficients[i][i];
+        }
+        return result;
     }
 
+    /**
+     * @param coefficients given matrix
+     * @param independentTerms results
+     * @return the solutions to the equations system using Gauss elimination without pivoting
+     */
     @Override
     public double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms) {
-        return new double[0];
+        for (int k = 0; k < coefficients.length; k++) {
+            double diagonal = coefficients[k][k];
+            for (int j = k; j < coefficients[0].length; j++) {
+                coefficients[k][j] = (coefficients[k][j] / diagonal);
+            }
+            independentTerms[k] = (independentTerms[k] / diagonal);
+            for (int i = k + 1; i < coefficients.length; i++) {
+                double multiplier = coefficients[i][k];
+                for (int j = k; j < coefficients[0].length; j++) {
+                    coefficients[i][j] = (coefficients[i][j] - multiplier * coefficients[k][j]);
+                }
+                independentTerms[i] = independentTerms[i] - multiplier * independentTerms[k];
+            }
+        }
+        return exercise1(coefficients, independentTerms);
     }
 
+    /**
+     * @param coefficients given matrix
+     * @param independentTerms results
+     * @return the solutions to the equations system using Gauss elimination with partial pivoting
+     */
     @Override
     public double[] exercise5PartialPivoteo(double[][] coefficients, double[] independentTerms) {
         int n = independentTerms.length;
-
         for (int p = 0; p < n; p++) {
             int max = p;
             for (int i = p + 1; i < n; i++) {
@@ -45,7 +86,6 @@ public class Guia7 implements TP4 {
             double t = independentTerms[p];
             independentTerms[p] = independentTerms[max];
             independentTerms[max] = t;
-
             for (int i = p + 1; i < n; i++) {
                 double alpha = coefficients[i][p] / coefficients[p][p];
                 independentTerms[i] -= alpha * independentTerms[p];
@@ -54,7 +94,6 @@ public class Guia7 implements TP4 {
                 }
             }
         }
-
         double[] x = new double[n];
         for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
@@ -66,61 +105,130 @@ public class Guia7 implements TP4 {
         return x;
     }
 
+    /**
+     * @param coefficients given matrix
+     * @param independentTerms results
+     * @param calculator to keep track of calculations
+     * @return the solutions to the equations system using Gauss elimination adopted to a superior Hessenberg matrix
+     */
     @Override
     public double[] exercise6(double[][] coefficients, double[] independentTerms, Calculator calculator) {
-        return new double[0];
+        for (int k = 0; k < coefficients.length; k++) {
+            double diagonal = coefficients[k][k];
+            for (int j = k; j < coefficients[0].length; j++) {
+                coefficients[k][j] = calculator.division(coefficients[k][j], diagonal);
+            }
+            independentTerms[k] = calculator.division(independentTerms[k], diagonal);
+            if (k + 1 < coefficients.length){
+                int i = k + 1;
+                double multiplier = coefficients[i][k];
+                for (int l = k; l < coefficients[0].length; l++) {
+                    coefficients[i][l] = calculator.subtraction(coefficients[i][l], calculator.multiplication(multiplier, coefficients[k][l]));
+                }
+                independentTerms[i] = calculator.subtraction(independentTerms[i], calculator.multiplication(multiplier, independentTerms[k]));
+            }
+        }
+        return exercise1(coefficients, independentTerms);
     }
 
+    /**
+     * @param coefficients given matrix
+     * @param independentTerms results
+     * @param calculator to keep track of calculations
+     * @return the solutions to the equations system using Gauss elimination adopted to the Spline method
+     */
     @Override
     public double[] exercise7(double[][] coefficients, double[] independentTerms, Calculator calculator) {
-        return new double[0];
+        coefficients[0][1] = calculator.division(coefficients[0][1], coefficients[0][0]);
+        independentTerms[0] = calculator.division(independentTerms[0], coefficients[0][0]);
+        for(int i = 1; i < coefficients.length - 1; i++){
+            coefficients[i][i + 1] = calculator.division(coefficients[i][i + 1], (calculator.subtraction(coefficients[i][i], calculator.multiplication(coefficients[i -1][i], coefficients[i][i - 1]))));
+            double numerator = calculator.subtraction(independentTerms[i], calculator.multiplication(independentTerms[i - 1],coefficients[i][i - 1]));
+            double denominator = calculator.subtraction(independentTerms[i], calculator.multiplication(coefficients[i - 1][i],coefficients[i][i - 1]));
+            independentTerms[i] = calculator.division(numerator, denominator);
+        }
+        for(int i = coefficients.length - 2; i >= 0; i--){
+            independentTerms[i] = independentTerms[i] - coefficients[i][i + 1] * independentTerms[i + 1];
+        }
+        return independentTerms;
     }
 
+    /**
+     * @param coefficients the given matrix
+     * @return the inverse of a matrix using Gauss-Jordan
+     */
     @Override
     public double[][] exercise8(double[][] coefficients) {
-        double[][] a = coefficients;
-        int e = 1;
-        int n = a.length;
-        for (int j = 0; j < n; j++) {
-            int p = j;
-            double max = Math.abs(a[p][0]);
-            for (int i = j + 1; i < n; i++) {
-                if (Math.abs(a[i][j]) > max) {
-                    p = i;
-
-                }
-
+        int rows = coefficients.length;
+        int columns = coefficients[0].length;
+        double[][] out = new double[rows][columns];
+        double[][] old = new double[rows][columns * 2];
+		double[][] newest = new double[rows][columns * 2];
+        for (int v = 0; v < rows; v++) {
+            for (int s = 0; s < columns * 2; s++) {
+                if (s - v == rows) old[v][s] = 1;
+                if(s < columns) old[v][s] = coefficients[v][s];
             }
-            System.out.println(p);
-            if (p > j) {
-                double temp = 0;
-                for (int i = 0; i < a[1].length; i++) {
-                    temp = a[j][i];
-                    a[j][i] = a[p][i];
-                    a[p][i] = temp;
-                }
-            }
-            double divisor = a[j][j];
-            for ( int i = 0; i < a[j].length; i ++){
-
-                a[j][i] = a[j][i]/divisor;
-            }
-            for (int i = 0; i < a.length; i++) {
-                if (i != j) {
-                    double multiple = a[i][j];
-                    for (int x = 0; x < a[1].length; x++) {
-                        a[i][x] = (a[i][x] - a[i][j] * a[j][x]);
-
-                    }
-                }
-            }
-
         }
-        return a;
+        for (int v = 0; v < rows; v++) {
+            for (int v1 = 0; v1 < rows; v1++) {
+                for (int s = 0; s < columns * 2; s++) {
+                    if (v == v1) newest[v][s] = old[v][s] / old[v][v];
+                    else newest[v1][s] = old[v1][s];
+                }
+            }
+            old = aux(newest);
+            for (int v1 = v + 1; v1 < rows; v1++) {
+                for (int s = 0; s < columns * 2; s++) {
+                    newest[v1][s] = old[v1][s] - old[v][s] * old[v1][v];
+                }
+            }
+            old = aux(newest);
+        }
+        for (int s = columns - 1; s > 0; s--) {
+            for (int v = s - 1; v >= 0; v--) {
+                for (int s1 = 0; s1 < columns * 2; s1++) {
+                    newest[v][s1] = old[v][s1] - old[s][s1] * old[v][s];
+                }
+            }
+            old = aux(newest);
+        }
+        for (int v = 0; v < rows; v++) {
+            System.arraycopy(newest[v], columns, out[v], 0, columns * 2 - columns);
+        }
+        return out;
     }
 
+    public double[][] aux(double[][] in) {
+        double[][] out = new double[in.length][in[0].length];
+        for(int v = 0; v < in.length; v++) {
+            System.arraycopy(in[v], 0, out[v], 0, in[0].length);
+        }
+        return out;
+    }
+
+    /**
+     * @param coefficients given matrix
+     * @param independentTerms results
+     * @return the solutions to the equations system using LU decomposition
+     */
     @Override
     public double[] exercise9(double[][] coefficients, double[] independentTerms) {
-        return new double[0];
+        double[][] U = new double[coefficients.length][coefficients[0].length];
+        double[][] L = new double[coefficients.length][coefficients[0].length];
+        for (int k = 0; k < coefficients.length; k++) {
+            for (int j = k; j < coefficients.length; j++) {
+                double u = 0;
+                double l = 0;
+                for (int p = 0; p < k; p++) {
+                    u += L[k][p] * U[p][j];
+                    l += L[j][p] * U[p][k];
+                }
+                U[k][j] = coefficients[k][j] - u;
+                L[j][k] = (coefficients[j][k] - l) / U[k][k];
+            }
+        }
+        double[] z = exercise2(L, independentTerms);
+        return exercise5WithoutPivoteo(U, z);
     }
 }
